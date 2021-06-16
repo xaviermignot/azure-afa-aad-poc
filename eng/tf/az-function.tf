b6bc2a107c14551ea63b194b38dc282e6f39f0e6
Xavier Mignot <xavier.mignot@xmi.fr>
2021-06-16 18:25:07 +0200

    Add easy auth

--- eng/tf/az-function.tf
+++ eng/tf/az-function.tf
@@ -19,11 +19,29 @@ resource "azurerm_function_app" "afa_app" {
   storage_account_name = azurerm_storage_account.storage_afa.name
   storage_account_access_key = azurerm_storage_account.storage_afa.primary_access_key
 
+  version = "~3"
+
   identity {
     type = "SystemAssigned"
   }
 
+  auth_settings {
+    enabled = true
+    runtime_version = "v2"
+    unauthenticated_client_action = "RedirectToLoginPage"
+    active_directory {
+      client_id = azuread_application.ad_app.application_id
+      client_secret = azuread_application_password.ad_app_secret.value
+    }
+  }
+
   app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.ai.instrumentation_key
+    "FUNCTIONS_WORKER_RUNTIME" = "dotnet"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+
+    "AzureAd:ClientId" = azuread_application.ad_app.application_id
+    "AzureAd:ClientSecret" = azuread_application_password.ad_app_secret.value
+    "AzureAd:TenantId" =  data.azurerm_client_config.current.tenant_id
   }
 }
